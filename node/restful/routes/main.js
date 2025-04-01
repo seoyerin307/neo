d8ccf65268522f1c65f9a48000d233074c962ebb
seoyerin307 <yr11220@naver.com>
2025-04-01 18:23:03 +0900

    restful api

--- node/restful/routes/main.js
+++ node/restful/routes/main.js
@@ -8,77 +8,84 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const users = [
-    {id:1, name: 'User1'},
-    {id:2, name: 'User2'},
-    {id:3, name: 'User3'},
+const users =[
+    {id: 1, name: 'User1'},
+    {id: 2, name: 'User2'},
+    {id: 3, name: 'User3'}
 ]
 
 app.get('/Hello', (req, res) => {
     res.send('Hello World!!')
 })
 
+// request X, response O
 app.get('/api/users', (req, res) => {
     res.json({ok: true, users: users})
 })
 
+// query string, request O, response O
 app.get('/api/users/user', (req, res) => {
     let user = "";
-    const { user_id, name } = req.query;
+    const { id, name } = req.query;
 
     if (req.query.name == null) {
-        user = users.filter(data => data.id == user_id);
+        user = users.filter(data => data.id == id);
     } else {
-        user = users.filter(data => data.id == user_id && data.name == name);
+        user = users.filter(data => data.id == id && data.name == name);
     }
     res.json({ok: false, users: user})
 })
 
+// query params(path param), request O, response O
 app.get('/api/users/:id', (req, res) => {
-    let user_id = req.params.id
+    let id = req.params.id
 
-    const user = users.filter(data => data.id == user_id);
+    const user = users.filter(data => data.id == id);
     res.json({ok: false, users: user})
 })
 
+// post, request body O, response O
 app.post('/api/users/userBody', (req, res) => {
     const id = req.body.id;
-    const user  = users.filter(data => data.id == id);
+    const user = users.filter(data => data.id == id);
     res.json({ok: false, users: user})
 })
 
+// post, request body O, response O
 app.post('/api/users/add', (req, res) => {
     const { id, name } = req.body;
     const user = users.concat({id, name});
     res.json({ok: true, users: user})
 })
-    
 
+// put, request body O, response O
 app.put('/api/users/update', (req, res) => {
     const { id, name } = req.body;
-    const user = users.map( data => {
-       if (data.id == id) data.name = name
-       return {
-           id: data.id,
-           name: data.name
-       }
+    const user = users.map(data => {
+        if (data.id == id) data.name = name
+        return {
+            id: data.id,
+            name: data.name
+        } 
     })
     res.json({ok: true, users: user})
 })
 
+// patch, request params & body O, response O
 app.patch('/api/users/update/:id', (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
-    const user = users.map( data => {
-       if (data.id == id) data.name = name
-       return {
-           id: data.id,
-           name: data.name
-       }
+    const user = users.map(data => {
+        if (data.id == id) data.name = name
+        return {
+            id: data.id,
+            name: data.name
+        } 
     })
     res.json({ok: true, users: user})
 })
 
+// delete, request body O, response O
 app.delete('/api/users/delete', (req, res) => {
     const { id } = req.body;
     const user = users.filter(data => data.id != id);
