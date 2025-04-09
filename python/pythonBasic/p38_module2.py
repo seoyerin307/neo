70da755021b3600393bfef7de962ccdbb15d193f
seoyerin307 <yr11220@naver.com>
2025-04-09 17:50:55 +0900

    add study: python module

---
+++ python/pythonBasic/p38_module2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import minimum as Min
+
+a = int(input("Enter a number : "))
+b = int(input("Enter a another number : "))
+
+print(f'Min Number is {Min.min(a, b)}')
---
+++ python/pythonBasic/p38_module3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from minimum import min
+
+a = int(input("Enter a number : "))
+b = int(input("Enter a another number : "))
+
+print(f'Min Number is {min(a, b)}')
