a7197b981dfef1d4e8a9b888b7a8d2d82b072a9b
seoyerin307 <yr11220@naver.com>
2025-04-14 17:54:51 +0900

    python crawling

---
+++ python/pythonData/p297_patternMatch.py
@@ -0,0 +1,19 @@
+import re
+
+mylist = ['abc123', 'cd456', 'ef789', 'abc12']
+
+regex = '[a-z{2}\d{3}]'
+
+pattern = re.compile(regex)
+
+print('# 문자열 2개, 숫자 3개 패턴 찾기')
+totallist = []
+for item in mylist:
+    if pattern.match(item):
+        print(item, '은(는) 조건에 적합')
+        totallist.append(item)
+    else:
+        print(item, '은(는) 조건에 적합하지 않음')
+
+    print('\n# 조건에 적합한 항목들')
+    print(totallist)
