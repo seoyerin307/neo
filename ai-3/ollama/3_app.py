4c36650802c39bcabd66cdb107bca42cef82bd46
seoyerin307 <yr11220@naver.com>
2025-06-05 17:51:28 +0900

    ollama

---
+++ ai-3/ollama/3_app.py
@@ -0,0 +1,14 @@
+import ollama
+import os
+
+img_path = './data/image.png' 
+print(os.path.exists(img_path))
+
+response = ollama.chat(
+    model="llama3.2:latest",    
+    messages=[
+        {"role": "user", "content": "사진 속의 승용차의 차종과 번호를 알려줘 ", "image": [img_path]}
+    ]   
+)
+
+print(response['message']['content'])
